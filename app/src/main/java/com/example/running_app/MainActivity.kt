24108df61f7fa57ad4e0cc84ba066d293e3b554f
Boris Hoi <Boris.Hoi@metropolia.fi>
2022-09-24 04:18:06 +0300

    Drafted Colors and Added Time and Date

--- app/src/main/java/com/example/running_app/MainActivity.kt
+++ app/src/main/java/com/example/running_app/MainActivity.kt
@@ -3,14 +3,21 @@ package com.example.running_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.running_app.ui.theme.Running_AppTheme
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,23 +25,59 @@ class MainActivity : ComponentActivity() {
         setContent {
             Running_AppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MainView()
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun MainView() {
+    val navController = rememberNavController()
+    val sdf1 = SimpleDateFormat("dd/MM/yyyy")
+    val sdf2 = SimpleDateFormat("HH:mm")
+    val date = sdf1.format(Date())
+    val time = sdf2.format(Date())
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        Text(text = date)
+        Text(text = time)
+        NavHost(navController, startDestination = "home") {
+            composable("home") {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Running_AppTheme {
-        Greeting("Android")
+            }
+            composable("weather") {
+
+            }
+            composable("suggestedTracks") {
+
+            }
+            composable("stats") {
+
+            }
+            composable("training") {
+
+            }
+            composable("startRunning") {
+
+            }
+        }
     }
 }
+//@Composable
+//fun Greeting(name: String) {
+//    Text(text = "Hello $name!")
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    Running_AppTheme {
+//        Greeting("Android")
+//    }
+//}
--- app/src/main/java/com/example/running_app/ui/theme/Color.kt
+++ app/src/main/java/com/example/running_app/ui/theme/Color.kt
@@ -2,7 +2,15 @@ package com.example.running_app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Default Colors
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+// Colors
+val AeroBlue = Color(0xFFDAFFEF)
+val Orange = Color(0xFFED6A5A)
+val Blue = Color(0xFF3A7CA5)
+val Cadet = Color(0xFF2B2D42)
+val OBlue = Color(0xFF0A1128)
